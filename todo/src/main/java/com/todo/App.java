fa4d78621093b213a06cb73d475b49ae2118e0e7
climaxd <b63822751@gmail.com>
2025-10-15 23:51:00 -0400

    probando la wea

---
+++ todo/src/main/java/com/todo/App.java
@@ -0,0 +1,16 @@
+package com.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("chupala we wes");
+        System.out.println("cmabiando weas");
+        System.out.println("anotherone");
+    }
+}
